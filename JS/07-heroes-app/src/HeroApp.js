b6fb4fd51463cf94890e142e2cef934b8f3b39c5
rigoriosh <rigoriosh@gmail.com>
2021-11-03 18:03:40 -0500

    03112021

--- JS/07-heroes-app/src/HeroApp.js
+++ JS/07-heroes-app/src/HeroApp.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import { AuthContext } from './auth/AuthContext'
 import { authReducer } from './auth/authReducer'
 import { AppRouter } from './routers/AppRouter'
@@ -10,14 +10,50 @@ const init = () => {
 export const HeroApp = () => {
 
     const [user, dispatch] = useReducer(authReducer, {}, init)
+    const [myDevice, setMyDevice] = useState();
 
     useEffect(() => {
         console.log('in here')
         localStorage.setItem('user', JSON.stringify(user))
     }, [user])
 
+    const connectButtonPointerUpHandler = () => {
+        navigator.bluetooth.requestDevice({
+          filters:
+            [
+              { name: 'HUAWEI Y9 Prime 2019 rrh' },
+            //   { services: [SEND_SERVICE] },
+            ]
+        })
+          .then(device => {
+            setMyDevice(device);
+      
+            return device.gatt.connect();
+          })
+          .then(server => {
+            //   server.getPrimaryService(SEND_SERVICE)
+            console.log({server});
+              
+          }
+          )
+        //   .then(service => service.getCharacteristic(SEND_SERVICE_CHARACTERISTIC))
+        //   .then(characteristic => {
+        //     toggleLigthCharacteristic = characteristic;
+      
+        //     toggleButtonsVisible();
+        //     toggleItemsEventListeners('addEventListener');
+        //   })
+          .catch(error => {
+            console.error(error);
+          });
+
+        }
+
+    console.log({myDevice});
+
     return (
         <AuthContext.Provider value={{user, dispatch}}>
+            <button onClick={()=>connectButtonPointerUpHandler()}>sss</button>
             <AppRouter/>
         </AuthContext.Provider>
         )
